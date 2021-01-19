074807d304f298850f55199f38cd2a4656a2102e
Max Belushkin <belushkin@gmail.com>
2021-01-19 16:30:59 +0200

    Added new template

--- next.config.js
+++ next.config.js
@@ -1,10 +1,26 @@
-module.exports = {
-  target: 'serverless',
-  webpack: function (config) {
-    config.module.rules.push({
-      test: /\.md$/,
-      use: 'raw-loader',
-    })
-    return config
+const withMdxEnhanced = require("next-mdx-enhanced");
+const rehypePrism = require("@mapbox/rehype-prism");
+
+module.exports = withMdxEnhanced({
+  layoutPath: "src/layouts",
+  defaultLayout: true,
+  rehypePlugins: [rehypePrism],
+})({
+  pageExtensions: ["mdx", "tsx"],
+  webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
+    config.module.rules.push(
+      ...[
+        {
+          test: /\.yml$/,
+          type: "json",
+          use: "yaml-loader",
+        },
+        {
+          test: /\.svg$/,
+          use: "@svgr/webpack",
+        },
+      ]
+    );
+    return config;
   },
-}
+});
