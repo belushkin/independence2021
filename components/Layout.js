1045fabca0bb84452750a7ae997e0e11fa986459
Max Belushkin <belushkin@gmail.com>
2021-01-19 15:37:50 +0200

    Added new blog

--- components/Layout.js
+++ components/Layout.js
@@ -12,7 +12,7 @@ export default function Layout({ children, pageTitle, description, ...props }) {
         <title>{pageTitle}</title>
       </Head>
       <style jsx global>{`
-        @import url('https://fonts.googleapis.com/css2?family=Inter:wght@300;400;700;800;900&display=swap');
+        @import url('https://fonts.googleapis.com/css2?family=Inter:wght@400&display=swap');
         html,
         body {
           margin: 0;
