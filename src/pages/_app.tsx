b2d28d0a40f28b5e576682ba559fb9af0093ebdd
Max Belushkin <belushkin@gmail.com>
2021-01-25 15:20:24 +0200

    Added initial redux support

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,3 +1,6 @@
+import { Provider } from 'react-redux'
+import { useStore } from '@reducers/store'
+
 import "normalize.css";
 import { AppProps } from "next/app";
 // NOTE: Do not move the styles dir to the src.
@@ -5,5 +8,11 @@ import { AppProps } from "next/app";
 import "../../public/styles/global.css";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  const store = useStore(pageProps.initialReduxState)
+  
+  return (
+    <Provider store={store}>
+      <Component {...pageProps} />
+    </Provider>
+  )
 }
